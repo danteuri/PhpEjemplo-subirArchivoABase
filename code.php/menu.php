05ec3a8aba5e57b27d5dfd243b34aba36ca7da46
lucas <santiagomaytahuanca@gmail.com>
2023-09-04 16:37:51 -0300

    a
    a

--- code.php/menu.php
+++ code.php/menu.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Menu general</title>
-	<link rel="stylesheet" href="css/menu.css">
+	<link rel="stylesheet" href="../css/menu.css">
 </head>
 <body>
 	<div class="header">
@@ -15,7 +15,7 @@
 	<a href="forms/form-altas.php">Alta</a><br>
 	<a href="forms/form-bajas.php">Bajas</a><br>
 	<a href="forms/form-modificacion.php">Modificaciones</a><br>
-	<a href="listar.php">Listado de personas</a><br>
+	<a href="../code.php/modificacion.php">Listado de personas</a><br>
 
 	</div>
 	</div>
--- index.php
+++ index.php
@@ -1,4 +1,53 @@
 <?php
-include '../PhpEjemplo-subirArchivoABase/code.php/menu.php';
+if (isset($_GET['page'])) {
+    $page = $_GET['page'];
+    $allowed_pages = [
+        'altas',
+        'bajas',
+        'form-altas',
+        'form-bajas',
+        'form-modificacion',
+        'listar',
+        'menu',
+        'modificacion'
+    ];
 
+    if (in_array($page, $allowed_pages)) {
+        $php_file = "php/$page.php";
+        if (file_exists($php_file)) {
+            include $php_file;
+        } else {
+            echo "Archivo no encontrado.";
+        }
+    } else {
+        echo "Acceso no autorizado.";
+    }
+} else {
+    echo "Página de inicio";
+}
 ?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu general</title>
+    <link rel="stylesheet" href="css/menu.css">
+</head>
+<body>
+    <div class="header">
+        <h3 class="titulo">Menu General</h3>
+
+        <div class="menu">       
+
+        <a href="?page=form-altas">Alta</a><br>
+        <a href="?page=form-bajas">Bajas</a><br>
+        <a href="?page=form-modificacion">Modificaciones</a><br>
+        <a href="?page=listar">Listado de personas</a><br>
+
+
+        </div>
+    </div>
+</body>
+</html>
