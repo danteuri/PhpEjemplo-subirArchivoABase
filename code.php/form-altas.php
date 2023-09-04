5301652d4a7d950873cc93f54eb8de05760dd0cc
lucas <santiagomaytahuanca@gmail.com>
2023-09-04 17:48:23 -0300

    rthr
    rth

--- code.php/form-altas.php
+++ code.php/form-altas.php
@@ -1,5 +1,5 @@
 <?php
-include 'menu.php'
+include 'index.php'
 ?>
 <!DOCTYPE html>
 <html lang="es">
--- index.php
+++ index.php
@@ -2,8 +2,11 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="Expires" content="0">
+	<meta http-equiv="Last-Modified" content="0">
+	<meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
+	<meta http-equiv="Pragma" content="no-cache">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
     <link rel="stylesheet" href="css/menu.css">
 </head>
 <body>
