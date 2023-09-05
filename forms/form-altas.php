a3112b892e4f3ae3ba5c58cd85e72215e4b8bbea
danteuri <santiagomaytahuanca@gmail.com>
2023-09-05 00:19:04 -0300

    comienzo
    pude entender por donde empezar, el body del menu porque ahi se aplica a todo y bueno luego de eso solamnete tengo sue;o asique me ire a dormir Zzz

--- forms/form-altas.php
+++ forms/form-altas.php
@@ -7,6 +7,7 @@ include '../code/menu.php';
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="../css/formAltas.css">
+	<link rel="stylesheet" href="../css/menu.css">
 	<title>Formulario de Altas</title>
 </head>
 <body>
